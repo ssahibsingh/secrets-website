63086a16023244b62c180f0aba0ef5a94f67221b
Sahib Singh <75523820+ssahibsingh@users.noreply.github.com>
2022-02-15 23:32:56 +0530

    level 1

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://admin-exnone:nNone2019@ay-secrets-website.dxwvh.mongodb.net/userDB");
 
 const app = express();
 
@@ -8,11 +11,46 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const userSchema = mongoose.Schema({
+    email: String,
+    password: String,
+})
+
+const User = mongoose.model('User', userSchema);
+
+
+
 
 app.get('/', (req, res) => {
     res.render('home');
 })
+app.get('/login', (req, res) => {
+    res.render('login');
+})
+app.get('/register', (req, res) => {
+    res.render('register');
+})
 
+app.get('/submit', (req, res) => {
+    res.render('submit');
+})
+
+
+app.post("/register", (req,res)=>{
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    })
+
+    newUser.save((err)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.render("secrets")
+        }
+    })
+    
+})
 
 app.listen(process.env.PORT || 3000, ()=>{
     console.log('Successfully Listening');
